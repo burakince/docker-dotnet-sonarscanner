3a33f28fd15ad0b47d119c38b665c22bb7e0d789
Burak Ince <bince@thoughtworks.com>
2018-04-01 00:10:49 +0200

    Fix scanner exe name

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ PROJECT_VERSION="${PROJECT_VERSION:-1.0}"
 SONAR_HOST="${HOST:-http://localhost:9000}"
 SONAR_LOGIN_KEY="${LOGIN_KEY:-admin}"
 
-mono /opt/sonar-scanner-msbuild/SonarQube.Scanner.MSBuild.exe begin /d:sonar.host.url=$SONAR_HOST /d:sonar.login=$SONAR_LOGIN_KEY /k:$PROJECT_KEY /n:"$PROJECT_NAME" /v:$PROJECT_VERSION
+mono /opt/sonar-scanner-msbuild/SonarScanner.MSBuild.exe begin /d:sonar.host.url=$SONAR_HOST /d:sonar.login=$SONAR_LOGIN_KEY /k:$PROJECT_KEY /n:"$PROJECT_NAME" /v:$PROJECT_VERSION
 dotnet restore
 dotnet build
 dotnet test
-mono /opt/sonar-scanner-msbuild/SonarQube.Scanner.MSBuild.exe end /d:sonar.login=$SONAR_LOGIN_KEY
+mono /opt/sonar-scanner-msbuild/SonarScanner.MSBuild.exe end /d:sonar.login=$SONAR_LOGIN_KEY
